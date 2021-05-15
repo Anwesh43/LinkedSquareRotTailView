2878e28ddb7be726e928450725a349bf6bf8e08e
Anwesh43 <anweshthecool0@gmail.com>
2021-05-16 00:50:11 +0530

    adding Canvas extension methods

--- squarerottailview/src/main/java/com/example/squarerottailview/SquareRotTailView.kt
+++ squarerottailview/src/main/java/com/example/squarerottailview/SquareRotTailView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.02f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 3.9f
@@ -30,3 +30,32 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSquareRotTail(scale : Float, w : Float, h : Float, paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val sf5 : Float = sf.divideScale(4, parts)
+    val x : Float = (w / 2 - size) * (1 - sf3 + sf5)
+    save()
+    translate(w / 2, h / 2)
+    rotate(deg * sf4)
+    save()
+    drawLine(0f, 0f, x * sf2, 0f, paint)
+    translate(x, 0f)
+    drawRect(RectF(size - size * sf1, -size / 2, size, size / 2), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawSRTNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSquareRotTail(scale, w, h, paint)
+}

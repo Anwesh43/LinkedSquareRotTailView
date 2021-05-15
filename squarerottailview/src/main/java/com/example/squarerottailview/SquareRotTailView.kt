d8ffe25e6851df84095c113ff20725ce4cb6a3e9
Anwesh43 <anweshthecool0@gmail.com>
2021-05-16 01:01:04 +0530

    adding SRTNode class

--- squarerottailview/src/main/java/com/example/squarerottailview/SquareRotTailView.kt
+++ squarerottailview/src/main/java/com/example/squarerottailview/SquareRotTailView.kt
@@ -122,4 +122,45 @@ class SquareRotTailView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SRTNode(var i : Int, val state : State = State()) {
+
+        private var next : SRTNode? = null
+        private var prev : SRTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SRTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSRTNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SRTNode {
+            var curr : SRTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }

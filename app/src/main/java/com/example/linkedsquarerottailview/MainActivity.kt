ca241cc3da58844e4863fd253ea5d1470c601a3a
Anwesh43 <anweshthecool0@gmail.com>
2021-05-16 01:13:34 +0530

    adding static create method

--- app/src/main/java/com/example/linkedsquarerottailview/MainActivity.kt
+++ app/src/main/java/com/example/linkedsquarerottailview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedsquarerottailview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.squarerottailview.SquareRotTailView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareRotTailView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }

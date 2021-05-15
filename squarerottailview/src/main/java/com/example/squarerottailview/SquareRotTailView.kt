706b7f022ec5918fbb8c005efd3607f4edb748d5
Anwesh43 <anweshthecool0@gmail.com>
2021-05-16 00:42:27 +0530

    adding constants and libraries

---
+++ squarerottailview/src/main/java/com/example/squarerottailview/SquareRotTailView.kt
@@ -0,0 +1,28 @@
+package com.example.squarerottailview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#3F51B5",
+    "#00C853",
+    "#304FFE",
+    "#880E4F"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
+
